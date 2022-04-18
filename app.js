1052c60a1943fecc9580b24e08fa419ca44e507f
Lindsey Myers <candlmyers1012@gmail.com>
2022-04-18 17:01:01 -0600

    add routes

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
 app.listen(port, () => {
-  console.log('Running on port ${port)');
+  console.log(`Running on port ${port}`);
 });
-
-
-
-
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello Whole World');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
