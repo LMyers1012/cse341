296e16d24ea2d74b06d91b650404ad160e54dafe
Lindsey Myers <candlmyers1012@gmail.com>
2022-04-25 12:27:29 -0600

    remove frontend, update filepath to new folder

--- backend/controllers/contacts.js
+++ backend/controllers/contacts.js
@@ -24,5 +24,76 @@ const getContactById = async (req, res, next) => {
   });
 };
 
+const createContact = async (req, res, next) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res
+      .status(500)
+      .json(response.error || 'An error occurred while creating the contact.');
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  const userIdToUpdate = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .replaceOne({ _id: userIdToUpdate }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || 'An error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  const userIdToDelete = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .remove({ _id: userIdToDelete }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || 'An error occurred while deleting the contact.');
+  }
+};
+
 // Export the functions
-module.exports = { getAllContacts, getContactById };
+module.exports = {
+  getAllContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  deleteContact,
+};
--- backend/routes/contacts.js
+++ backend/routes/contacts.js
@@ -4,5 +4,8 @@ const contactsController = require('../controllers/contacts');
 // Call functions based on url entered
 routes.get('/', contactsController.getAllContacts);
 routes.get('/:id', contactsController.getContactById);
+routes.post('/', contactsController.createContact);
+routes.put('/:id', contactsController.updateContact);
+routes.delete('/:id', contactsController.deleteContact);
 
 module.exports = routes;
