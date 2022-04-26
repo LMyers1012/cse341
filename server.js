c3229132131b17ca7dfc7eebb2d33829d615255f
Lindsey Myers <candlmyers1012@gmail.com>
2022-04-26 16:40:11 -0600

    add header 1 heroku css test

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader(
-    //   'Access-Control-Allow-Headers',
-    //   'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    // );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
     // res.setHeader('Content-Type', 'application/json');
     // res.setHeader(
     //   'Access-Control-Allow-Methods',
