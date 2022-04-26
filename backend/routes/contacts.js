2b5795092be73bb237c1be057cc993e4010a5c12
Lindsey Myers <candlmyers1012@gmail.com>
2022-04-26 13:50:03 -0600

    fixed package dependencies

--- backend/routes/contacts.js
+++ backend/routes/contacts.js
@@ -1,11 +1,11 @@
-const routes = require('express').Router();
+const router = require('express').Router();
 const contactsController = require('../controllers/contacts');
 
 // Call functions based on url entered
-routes.get('/', contactsController.getAllContacts);
-routes.get('/:id', contactsController.getContactById);
-routes.post('/', contactsController.createContact);
-routes.put('/:id', contactsController.updateContact);
-routes.delete('/:id', contactsController.deleteContactById);
+router.get('/', contactsController.getAllContacts);
+router.get('/:id', contactsController.getContactById);
+router.post('/', contactsController.createContact);
+router.put('/:id', contactsController.updateContact);
+router.delete('/:id', contactsController.deleteContactById);
 
-module.exports = routes;
+module.exports = router;
